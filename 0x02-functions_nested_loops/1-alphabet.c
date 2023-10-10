614b568b50aa9e38f39f53db2d8fb9a03942fab1
Zivha <zivha.beharie@gmail.com>
2023-10-10 06:10:33 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print lowercase alphabet
+ * Return: return 0
+ */
+
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a' ; x  <= 'z' ; x++)
+		_putchar(x);
+	_putchar('\n');
+}
