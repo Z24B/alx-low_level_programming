bed5ec622180b205f2597a9646e8da28a7e82812
Zivha <zivha.beharie@gmail.com>
2023-10-16 11:59:24 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - function that reverses a string
+ * @s: string that is reversed
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int x;
+	char y;
+	int count = 0;
+
+	for (x = 0 ; s[x] != '\0' ; x++)
+		count++;
+	for (x = 0 ; x < count / 2 ; x++)
+	{
+		y = s[x];
+		s[x] = s[count - 1 - x];
+		s[count - 1 - x] = y;
+	}
+}
