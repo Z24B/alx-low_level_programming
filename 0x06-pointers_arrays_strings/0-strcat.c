7ae7af3ff2704a409acab228968e103f73970a88
Zivha <zivha.beharie@gmail.com>
2023-10-18 04:51:39 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *_strcat - function that concatenates two strings
+ * _strcat - function that concatenates two strings
  * @dest: destination string value
  * @src: source string value
  * Return: pointer to the resulting string dest
