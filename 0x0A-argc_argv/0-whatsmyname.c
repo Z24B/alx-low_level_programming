0ac73cf00bfe1da3b6b1fd064be0d9b1c1086655
Zivha <zivha.beharie@gmail.com>
2023-10-27 20:57:21 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints group name
+ * @argc: parameter 1
+ * @argv: parameter 2
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
