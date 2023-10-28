4ccac324bfa2f20172c44d518b5b04895df193b3
Zivha <zivha.beharie@gmail.com>
2023-10-27 20:36:12 -0700

    Task 0

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - function that computes the absolute value of an integer
+ * @x: function parameter
+ * Return: -x or x;
+ */
+
+int _abs(int x)
+{
+	if (x >= 0)
+	{
+		return (x);
+	}
+	return (-x);
+}
