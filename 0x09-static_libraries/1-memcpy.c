1a7e8e4ea1e75c313912bc90f7fc45a195fdecc6
Zivha <144683073+Z24B@users.noreply.github.com>
2024-01-15 13:46:31 +0000

    1-memcpy.c
    Updated 15/01/2024

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
- * _memset - function that fills memory with a constant byte.
- * @s: original memory address
- * @b: constant byte value to be filled
- * @n: number of bytes
- * Return: pointer to the memory area s
+ * *_memcpy - copies memory area
+ * @dest: destination memory area
+ * @src: memory area to copy from
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest
  */
-
-char *_memset(char *s, char b, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int x;
+	unsigned int i;
 
-	for (x = 0; x < n; x++)
+	for (i = 0; i < n; i++)
 	{
-		s[x] = b;
+		dest[i] = src[i];
 	}
-	return (s);
+
+	return (dest);
 }
