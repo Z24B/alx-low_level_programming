a050b6ce3fe5542f0c89255220ed1d112758e8e9
Z24B <zivha.beharie@gmail.com>
2024-01-15 10:19:52 -0800

    Task 2

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+ * add - adds integers together
+ * @a: integer 1
+ * @b: integer 2
+ * Return: a+b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: a-b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: a*b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: a/b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - remainder of equation
+ * @a: integer 1
+ * @b: integer 2
+ * Return:a%b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
