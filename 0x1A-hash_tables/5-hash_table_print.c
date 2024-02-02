48a4e0526f5643558116f918ca852f877bba26f9
Z24B <zivha.beharie@gmail.com>
2024-02-02 04:04:08 -0800

    Task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+#include <stdio.h>
+
+/**
+ * hash_table_print - function that prints a hash table
+ * @ht: the hash table
+ * Return: If ht is NULL, don’t print anything
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	int flag = 0;
+	hash_node_t *temp;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		while (temp != NULL)
+		{
+			if (flag == 1)
+				printf(", ");
+			printf("'%s': '%s'", temp->key, temp->value);
+			flag = 1;
+			temp = temp->next;
+		}
+	}
+	printf("}\n");
+}
